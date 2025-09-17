410a81e57ac77688d8a7ba9ce9c48a49f580135c
Aman Kumar <amankumar2003.in@gmail.com>
2025-09-17 18:47:43 +0530

    deleted

--- 01_Basics/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-console.log(str1 + str2);
-
-// console.log("1" + 2); //12
-// console.log("1" + "2"); //12
-// console.log(1 + "2"); //12
-// console.log("1" + 2 + 2); //122
-// console.log(1 + 2 + "2"); //32
-
-// console.log(3 + 4 * 5 % 3); //5
