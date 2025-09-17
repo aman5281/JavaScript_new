bc8ee6fd3caa05d58e02d51168c6a3de52c9d656
Aman Kumar <amankumar2003.in@gmail.com>
2025-09-17 17:11:30 +0530

    added Basics Folder

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345678;
+let accountEmail = "amankumar2003.in@gmail.com";
+var accountPassword = "2334558";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 3344;    not allowed
+accountEmail = "hcl@gmail.com";
+accountPassword = "23456";
+accountCity = "Ambala";
+
+console.log(accountId);
+console.table([
+  accountEmail,
+  accountId,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
