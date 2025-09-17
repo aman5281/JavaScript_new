65a3b2e743bf65c6348f20e206a128a4300d7ab7
Aman Kumar <amankumar2003.in@gmail.com>
2025-09-17 17:48:14 +0530

    Datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3);  we are using nodeJS, not browser
+
+console.log(3+3);
+
+let name = "Aman Kumar";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+// number range 2^53
+// BigInt
+// String => ""
+// Boolean => true, false
+// null => standAlone Value
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
