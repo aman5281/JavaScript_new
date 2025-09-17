a4a3a3da04791658372e061a36f3b47a528f8806
Aman Kumar <amankumar2003.in@gmail.com>
2025-09-17 18:46:20 +0530

    Conversion Operation

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,60 @@
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// '33' => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+
+let isLoggedIn = "Aman";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// "1" => true
+// "0" => false
+// "Aman" => true
+// " " => false
+
+let someNumber = "Aman Kumar 333";
+let StingsomeNumber = String(someNumber);
+// console.log(StingsomeNumber);
+// console.log(typeof StingsomeNumber);
+
+
+// **************************Operations************************************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Aman";
+// console.log(str1 + str2);
+
+// console.log("1" + 2); //12
+// console.log("1" + "2"); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(3 + 4 * 5 % 3); //5
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
