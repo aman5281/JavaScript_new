bf34b30fae19f3de8ce7239e50c672a0f61f5a43
Aman Kumar <amankumar2003.in@gmail.com>
2025-09-18 18:40:23 +0530

    COmparison

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,15 @@
+console.log("2" > 1); //true
+console.log("02" > 1); //true
+
+console.log(null >= 0); //true
+console.log(null == 0); //false
+console.log(null < 0); //false
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+// === is known as strict bcz both the data types get check
+console.log("Strict Check");
+console.log("2" === 2);
+
