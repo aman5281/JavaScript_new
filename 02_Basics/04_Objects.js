461a67416e5f0e549f6c3880113983d3cb794255
Aman Kumar <amankumar2003.in@gmail.com>
2025-10-02 14:50:59 +0530

    04_Objects

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -8,48 +8,60 @@ tinderUser.isLoggedIn = false;
 // console.log(tinderUser);
 
 const regularUser = {
-  email : "amankumar33@gmail.com",
-  fullname : {
-    userFullName:{
-      firstName : "Aman",
-      lastName : "Kumar",
-    }
-  }
-}
+  email: "amankumar33@gmail.com",
+  fullname: {
+    userFullName: {
+      firstName: "Aman",
+      lastName: "Kumar",
+    },
+  },
+};
 
 // console.log(regularUser.fullname.userFullName.firstName);
 
-const obj1 = {1: 'a', 2: 'e'};
-const obj2 = {3: 'a', 4: 'e'};
-const obj3 = {5: 'a', 6: 'e'};
+const obj1 = { 1: "a", 2: "e" };
+const obj2 = { 3: "a", 4: "e" };
+const obj3 = { 5: "a", 6: "e" };
 
 // const obj4 = Object.assign({},obj1,obj2,obj3);
 
-const obj4 = {...obj1, ...obj2, ...obj3};
+const obj4 = { ...obj1, ...obj2, ...obj3 };
 // console.log(obj4);
 
 const user = [
   {
-    id : 1,
-    email : "amankumar2003.in@gmail.com"
+    id: 1,
+    email: "amankumar2003.in@gmail.com",
   },
   {
-    id : 2,
-    email : "amankumar2003.in@gmail.com"
+    id: 2,
+    email: "amankumar2003.in@gmail.com",
   },
   {
-    id : 3,
-    email : "amankumar2003.in@gmail.com"
+    id: 3,
+    email: "amankumar2003.in@gmail.com",
   },
-]
+];
 
 user[1].email;
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(Object.hasOwnProperty('isLoggedIn'));
+// console.log(Object.hasOwnProperty('isLoggedIn'));
 
 
+// ****************************OBJECT DE-STRUCTURE*************************
+const course = {
+  courseName: "JS Snippets",
+  price: 999,
+  cousreInstructor: "Aman Kumar",
+};
+
+// console.log(course.cousreInstructor);
+
+const { cousreInstructor: instructor } = course;
+// console.log(cousreInstructor);
+console.log(instructor);
